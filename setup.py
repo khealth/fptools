67cd293218a262aaa984b805494993b7d1af7b88
Iddan Aharonson <mail@iddan.co>
2017-10-17 16:37:28 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+
+setup(
+    name='fptools',
+    version='0.9.1',
+    description='Functional programming tools for Python',
+    url='https://github.com/kang-health/fptools',
+    author='Iddan Aharonson',
+    author_email='iddan@kanghealth.io',
+    install_requires=requirements
+)
