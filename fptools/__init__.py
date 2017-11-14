658ab82bfb3a6cfd3cf9e41cc7339121d4f548dd
Iddan Aharonson <mail@iddan.co>
2017-11-14 14:25:16 +0200

    collection in __init__.py

--- fptools/__init__.py
+++ fptools/__init__.py
@@ -1,4 +1,5 @@
 import fptools.callable as Callable
+import fptools.collection as Collection
 import fptools.dictionary as Dictionary
 import fptools.iterable as Iterable
 import fptools.sequence as Sequence
