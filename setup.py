38dda88aec0a01f05ab869f71a95c276ddd74860
Iddan Aharonson <mail@iddan.co>
2017-11-26 12:05:10 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fptools',
-    version='0.2.0',
+    version='0.2.1',
     description='Functional programming tools for Python',
     url='https://github.com/kang-health/fptools',
     author='Iddan Aharonson',
