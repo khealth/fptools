8878b40ebabab6f9b8738a9a2b15243e5986d777
Iddan Aaronsohn <mail@aniddan.com>
2019-02-18 00:09:54 +0200

    Updates

---
+++ docs/precache-manifest.fbeef19f7052e386efe6fc2c1d72b960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad9a2e96a5125d301bc",
+    "url": "/fptools/static/css/main.17aeb7f8.chunk.css"
+  },
+  {
+    "revision": "fad9a2e96a5125d301bc",
+    "url": "/fptools/static/js/main.fad9a2e9.chunk.js"
+  },
+  {
+    "revision": "3b772e746fe1792eacbe",
+    "url": "/fptools/static/js/runtime~main.3b772e74.js"
+  },
+  {
+    "revision": "5d50a445b07fe34f90e6",
+    "url": "/fptools/static/js/2.5d50a445.chunk.js"
+  },
+  {
+    "revision": "f76a07db3b82c8be241ce991b5db3a82",
+    "url": "/fptools/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1959dd16d928efa8203b4a18c1399218.js"
+  "/fptools/precache-manifest.fbeef19f7052e386efe6fc2c1d72b960.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fptools/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
