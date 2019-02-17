5df79b43a4cb9c391ef732049a6dc352bb6fea91
Iddan Aaronsohn <mail@aniddan.com>
2019-02-18 00:06:53 +0200

    Updates

---
+++ docs/precache-manifest.1959dd16d928efa8203b4a18c1399218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af4fa770ff0f172bae6d",
+    "url": "/static/css/main.17aeb7f8.chunk.css"
+  },
+  {
+    "revision": "af4fa770ff0f172bae6d",
+    "url": "/static/js/main.af4fa770.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5d50a445b07fe34f90e6",
+    "url": "/static/js/2.5d50a445.chunk.js"
+  },
+  {
+    "revision": "6e3c1437289113a59c8ffed75b8cf8cf",
+    "url": "/index.html"
+  }
+];
