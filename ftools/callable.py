151a9b3212a8613a5617ab5e5bc8803b763d2eda
Iddan Aaronsohn <mail@aniddan.com>
2020-01-27 01:17:04 +0200

    Add types for debounce and fix formatting

--- ftools/callable.py
+++ ftools/callable.py
@@ -7,7 +7,7 @@ from functools import partial, reduce, wraps
 from inspect import Parameter, getmodule, signature
 from logging import getLogger
 from time import time
-from typing import Callable, Iterable, Type, TypeVar, cast
+from typing import Callable, Iterable, Type, TypeVar, Union, cast
 
 
 def fullname(func: Callable) -> str:
@@ -215,9 +215,18 @@ def once(_callable: T) -> T:
 
 
 @curry
-def debounce(wait, func, leading=False, trailing=True):
+def debounce(
+    wait: Union[float, int], func: T, leading: bool = False, trailing: bool = True
+) -> T:
     """
-    Creates a debounced function that delays invoking func until after wait milliseconds have elapsed since the last time the debounced function was invoked. The debounced function comes with a cancel method to cancel delayed func invocations and a flush method to immediately invoke them. Provide options to indicate whether func should be invoked on the leading and/or trailing edge of the wait timeout. The func is invoked with the last arguments provided to the debounced function. Subsequent calls to the debounced function return the result of the last func invocation.
+    Creates a debounced function that delays invoking func until after wait
+    milliseconds have elapsed since the last time the debounced function was
+    invoked. The debounced function comes with a cancel method to cancel delayed
+    func invocations and a flush method to immediately invoke them. Provide
+    options to indicate whether func should be invoked on the leading and/or
+    trailing edge of the wait timeout. The func is invoked with the last
+    arguments provided to the debounced function. Subsequent calls to the
+    debounced function return the result of the last func invocation.
     """
     invoked = None
     value = None
@@ -234,5 +243,7 @@ def debounce(wait, func, leading=False, trailing=True):
             if trailing:
                 value = func(*args, **kwargs)
         return value
+
+    decorated = cast(T, decorated)
 
     return decorated
