ae21e3eb0e270ff752daee1dc6c83eb4fc04aa9e
Iddan Aaronsohn <mail@aniddan.com>
2020-01-27 02:14:33 +0200

    Updates

---
+++ docs/precache-manifest.714c453fc460ae46fc5ac81c3341e4f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bea513f46917b204c24dc0628286940",
+    "url": "/fptools/index.html"
+  },
+  {
+    "revision": "5b3176c0952f30e6906f",
+    "url": "/fptools/static/css/main.0ab6b5a4.chunk.css"
+  },
+  {
+    "revision": "bb5f12c4d0a4caf27e7e",
+    "url": "/fptools/static/js/2.87c44fd6.chunk.js"
+  },
+  {
+    "revision": "d8acfc518a74bbee4a2a25e3f2733ff1",
+    "url": "/fptools/static/js/2.87c44fd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b3176c0952f30e6906f",
+    "url": "/fptools/static/js/main.07aeac04.chunk.js"
+  },
+  {
+    "revision": "62050d30f7dc9754c5b3",
+    "url": "/fptools/static/js/runtime-main.3c639cd1.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fptools/precache-manifest.5afd5e10895b6b8f96d616bdd5dd9ba5.js"
+  "/fptools/precache-manifest.714c453fc460ae46fc5ac81c3341e4f6.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/fptools/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fptools/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
